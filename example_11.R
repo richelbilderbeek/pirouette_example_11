647dbb90866787fe06ca0f781985b63634c65f5d
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-02 13:46:09 +0100

    Use of 'pir_save'

--- example_11.R
+++ example_11.R
@@ -4,47 +4,35 @@
 # - no twinning
 #
 # Works under Linux and MacOS only
-
 library(pirouette)
 
-################################################################################
 # Constants
-################################################################################
 is_testing <- is_on_travis()
 example_no <- 11
 rng_seed <- 314
 folder_name <- paste0("example_", example_no, "_", rng_seed)
 
-################################################################################
 # Create phylogeny
-################################################################################
 set.seed(rng_seed)
 phylogeny <- create_yule_tree(n_taxa = 6, crown_age = 10)
 
-################################################################################
 # Setup pirouette
-################################################################################
 pir_params <- create_std_pir_params(
   folder_name = folder_name
 )
 pir_params$twinning_params <- NA
 stop("Remove generative model")
-
 if (is_testing) {
   pir_params <- shorten_pir_params(pir_params)
 }
 
-################################################################################
 # Run pirouette
-################################################################################
 pir_out <- pir_run(
   phylogeny,
   pir_params = pir_params
 )
 
-################################################################################
 # Save results
-################################################################################
 pir_save(
   phylogeny = phylogeny,
   pir_params = pir_params,
